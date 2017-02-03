3836fb48e82c4d84bb5b1ce9cb75217c57fa62a1
ravi523096 <ravi2523096@gmail.com>
2017-02-03 01:48:07 -0500

    Mocking in integration test cases

--- src/test/java/in/ravikalla/config/TestConfiguration.java
+++ src/test/java/in/ravikalla/config/TestConfiguration.java
@@ -1,12 +1,20 @@
 package in.ravikalla.config;
 
+import org.mockito.Mockito;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import in.ravikalla.repositories.PersonRepository;
+
 @EnableWebMvc
 @Configuration
 @ComponentScan(basePackages={    
-    "in.ravikalla"})
+    "in.ravikalla.controllers", "in.ravikalla.service"})
 public class TestConfiguration {
+	@Bean
+	public PersonRepository personRepository() {
+		return Mockito.mock(PersonRepository.class);
+	}
 }
--- src/test/java/in/ravikalla/controllers/PersonControllerTest.java
+++ src/test/java/in/ravikalla/controllers/PersonControllerTest.java
@@ -15,20 +15,15 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.client.RestOperations;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.google.gson.reflect.TypeToken;
@@ -37,15 +32,14 @@ import in.ravikalla.bean.Company;
 import in.ravikalla.bean.Person;
 import in.ravikalla.config.TestConfiguration;
 import in.ravikalla.repositories.PersonRepository;
-import in.ravikalla.service.PersonService;
 import in.ravikalla.utils.TestUtils;
 
 /**
- * 
  * @author ravi kalla
- * @see <a href="http://www.bytestree.com/">BytesTree</a>
+ * @since 1-Feb-2017
  *
  */
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = { TestConfiguration.class })
 @WebAppConfiguration
@@ -56,9 +50,6 @@ public class PersonControllerTest {
 	private MockMvc mockMvc;
 
 	@Autowired
-	private PersonService personService;
-
-	@Mock
 	private PersonRepository personRepository;
 
 	@Autowired
@@ -67,26 +58,29 @@ public class PersonControllerTest {
 	private final String URL_PERSONS = "/persons";
 	private final String URL_PERSON = "/person?id=test";
 
-	@Mock
+	
 	private static Person objPerson5;
-	@Mock
+	
 	private static Person objPerson6;
 
 	@Before
 	public void setUp() {
+		l.info("Start : PersonControllerTest.setUp()");
+
 		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
 
 		objPerson5 = stubPersons(5);
 		objPerson6 = stubPersons(6);
 
-		when(personService.findById(any(String.class))).thenReturn(objPerson5);
-		when(personService.findAll()).thenCallRealMethod();
 		when(personRepository.findAll()).thenReturn(Arrays.asList(objPerson5));
+		when(personRepository.findById(any(String.class))).thenReturn(objPerson6);
+
+		l.info("End : PersonControllerTest.setUp()");
 	}
 
 	@Test
-	public void testFindAll() throws Exception {
-		l.info("Start : PersonControllerTest.testFindAll()");
+	public void testPersonFindAll() throws Exception {
+		l.info("Start : PersonControllerTest.testPersonFindAll()");
 
 		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post(URL_PERSONS)
 				.contentType(MediaType.APPLICATION_JSON_UTF8).accept(MediaType.APPLICATION_JSON_UTF8).content(TestUtils.objectToJson(objPerson6))).andReturn();
@@ -101,12 +95,12 @@ public class PersonControllerTest {
 		Type listType = new TypeToken<ArrayList<Person>>(){}.getType();
 
 		List<Person> lstPersons = TestUtils.jsonToObject(result.getResponse().getContentAsString(), listType);
-		l.info("Returned list size : " + lstPersons.size());
+		l.info("End : PersonControllerTest.testPersonFindAll() : " + lstPersons.size());
 	}
 
 	@Test
-	public void testFindById() throws Exception {
-		l.info("Start : PersonControllerTest.testFindById()");
+	public void testPersonFindById() throws Exception {
+		l.info("Start : PersonControllerTest.testPersonFindById()");
 
 		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post(URL_PERSON)
 				.contentType(MediaType.APPLICATION_JSON_UTF8).accept(MediaType.APPLICATION_JSON_UTF8).content(TestUtils.objectToJson(objPerson6))).andReturn();
@@ -119,7 +113,8 @@ public class PersonControllerTest {
 		verify(personRepository).findById(any(String.class));
 
 		Person objPersons = TestUtils.jsonToObject(result.getResponse().getContentAsString(), Person.class);
-		l.info("Returned list size : " + objPersons.getFirstName());
+
+		l.info("End : PersonControllerTest.testPersonFindById() : " + objPersons.getFirstName());
 	}
 
 	private Person stubPersons(int intCtr) {
